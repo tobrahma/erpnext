c5d30400f50e6d4b95c5f615ea715e10c490fe2d
Brahma K <tobrahma@yahoo.com>
2011-08-02 14:13:22 +0530

    Test Loan modified

--- accounts/doctype/loan/test_loan.py
+++ accounts/doctype/loan/test_loan.py
@@ -5,13 +5,14 @@ from webnotes.model.code import get_obj
 class TestScheduleGeneartion(unittest.TestCase):
 	def setUp(self):
 		webnotes.conn.begin()
+#		webnotes.conn.sql("USE abc")
 		# create a mock loan
-		self.loan = get_obj('Loan', 'LOAN00001')
-		
+		#self.loan = get_obj('Loan', 'LOAN00001')
+
 	def test_generation(self):
 		"test the genaration of loan installments"
 		self.loan.generate()
 		self.assertEqual(self.loan.get_installment_total(), self.loan.doc.loan_amount)
-		
+
 	def tearDown(self):
 		webnotes.conn.rollback()
